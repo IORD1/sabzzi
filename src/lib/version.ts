3e665aa45ad73bfc23335513241ed75f2050f42b
Prathmesh Ingole <pratham111ingole@gmail.com>
2025-10-19 14:56:43 +0530

    bump version

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated file - do not edit manually
 // Updated by scripts/version-sw.js during build
 
-export const APP_VERSION = '1.4.5';
-export const BUILD_DATE = '2025-10-19T09:21:37.047Z';
+export const APP_VERSION = '1.4.6';
+export const BUILD_DATE = '2025-10-19T09:26:32.295Z';
