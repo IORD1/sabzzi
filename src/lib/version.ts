55fec87faf0f796d0e176b833f4f72577b11b1e3
Prathmesh Ingole <pratham111ingole@gmail.com>
2025-10-19 15:52:44 +0530

    update

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -2,4 +2,4 @@
 // Updated by scripts/version-sw.js during build
 
 export const APP_VERSION = '1.4.7';
-export const BUILD_DATE = '2025-10-19T09:50:51.985Z';
+export const BUILD_DATE = '2025-10-19T10:21:46.576Z';
